1838b0939e8334ed8a93fc97e2a6723f4ac53461
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-20 19:44:16 +0530

    ws server creation, token verification and user id reterieval

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+import { WebSocketServer } from "ws";
 
 import router from "./routes/serverRoutes.js";
 
@@ -22,8 +24,28 @@ app.use("/", router);
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server runnng on: http://localhost:${PORT}`);
-    });
+    console.log("Database connected");
   })
   .catch((error) => console.log(error));
+
+const server = app.listen(PORT, () => {
+  console.log(`Server runnng on: http://localhost:${PORT}`);
+});
+
+const ws = new WebSocketServer({ server });
+
+ws.on("connection", (connection) => {
+  console.log("Web socket connected");
+  connection.on("message", async (data) => {
+    const message = JSON.parse(data);
+    if (message.type === "token") {
+      try {
+        const token = jwt.verify(message.secret, process.env.SECRET);
+        const userID = token.id;
+        console.log("USER ID: ", userID);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+});
