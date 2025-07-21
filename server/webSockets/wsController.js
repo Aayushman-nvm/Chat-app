8cc8ba89b759a85e129f27a7966da80abf461ecf
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-21 22:15:09 +0530

    Sending online clients to frontend

---
+++ server/webSockets/wsController.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+export function handleConnection(ws, wss) {
+  //ws is ur individual socket, wss is ur comeplete socket server
+  ws.on("message", async (data) => {
+    const message = JSON.parse(data);
+    if (message.type === "token") {
+      try {
+        const { id, name } = jwt.verify(message.secret, process.env.SECRET);
+        ws.id = id;
+        ws.name = name;
+        console.log(
+          [...wss.clients].map((c) => `UserID: ${c.id}, Name: ${c.name}`)
+        );
+        [...wss.clients].forEach((client) => {
+          const otherClients = [...wss.clients].filter((c) => c !== client).map(c=>({id:c.id, name:c.name}));
+          console.log("Other clients: ",otherClients);
+          client.send(
+            JSON.stringify({
+              type: "onlineClient",
+              clients:otherClients,
+            })
+          );
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+}
