098fd34e0c0159a357b3e27d438b3dd5491b59f9
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-23 00:27:17 +0530

    sending received messages to recipient

--- server/webSockets/wsController.js
+++ server/webSockets/wsController.js
@@ -4,7 +4,7 @@ export function handleConnection(ws, wss) {
   //ws is ur individual socket, wss is ur comeplete socket server
   ws.on("message", async (data) => {
     const message = JSON.parse(data);
-    if (message.type === "token") {
+    if (message.type === "token") {//token part
       try {
         const { id, name } = jwt.verify(message.secret, process.env.SECRET);
         ws.id = id;
@@ -13,12 +13,14 @@ export function handleConnection(ws, wss) {
           [...wss.clients].map((c) => `UserID: ${c.id}, Name: ${c.name}`)
         );
         [...wss.clients].forEach((client) => {
-          const otherClients = [...wss.clients].filter((c) => c !== client).map(c=>({id:c.id, name:c.name}));
-          console.log("Other clients: ",otherClients);
+          const otherClients = [...wss.clients]
+            .filter((c) => c !== client)
+            .map((c) => ({ id: c.id, name: c.name }));
+          console.log("Other clients: ", otherClients);
           client.send(
             JSON.stringify({
               type: "onlineClient",
-              clients:otherClients,
+              clients: otherClients,
             })
           );
         });
@@ -26,5 +28,16 @@ export function handleConnection(ws, wss) {
         console.log(error);
       }
     }
+    if (message.type === "message") {//sending message part
+      const { recipient, text, sender } = message;
+      [...wss.clients]
+        .filter((client) => client.id === recipient.id)
+        .forEach((client) =>
+          client.send(JSON.stringify({ type: "message", text: text, id: sender.id, name:sender.name }))
+        );
+
+      console.log("Fe message ", message);
+    }
+    console.log("Fe message in backend ", message);
   });
 }
