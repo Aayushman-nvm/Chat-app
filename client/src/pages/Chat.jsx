0aaceb539012dd113d9d0f8c5f2731e3e36bd8bb
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-24 01:45:56 +0530

    Receiving message from database (static)

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -25,9 +25,10 @@ function Chat() {
     };
     setMessages((prev) => [...prev, newMessage]);
     // TODO: Send message via websocket here
+    console.log("Selected Contact: ", selectedContact);
     socket.send(JSON.stringify({
       type: "message",
-      recipient: selectedContact,
+      recipient: { id: selectedContact.id, name: selectedContact.name },
       sender: { id: selfUser._id, name: selfUser.name },
       text: newMessage.text,
       timestamp: newMessage.timestamp,
@@ -45,12 +46,13 @@ function Chat() {
         setOnlineUsers(messageData.clients);
       } else if (messageData.type === "message") {
         const newMessage = {
-          sender: { id: messageData.id, name: messageData.name },
-          text: messageData.text, timestamp: Date.now()
+          sender: { id: messageData.message.sender?.id, name: messageData.message.sender?.name },
+          text: messageData.message.text, timestamp: Date.now()
         };
+        console.log("New Message: ",newMessage);
         setMessages((prev) => [...prev, newMessage]);
-        console.log(`Compareing: ${messageData.id===selfUser._id}`)
-        console.log("Incoming message from be to fe: ", messageData.text);
+        console.log(`Compareing: ${messageData.id === selfUser._id}`)
+        console.log("Incoming message from be to fe: ", messageData.message);
       } else {
         console.log("Message data: ", messageData);
       }
