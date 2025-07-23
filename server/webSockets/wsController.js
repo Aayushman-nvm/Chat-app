c8513c77dda6bc2a4ba18ecd79c192cb82848aed
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-24 01:47:47 +0530

    Modularized the code

--- server/webSockets/wsController.js
+++ server/webSockets/wsController.js
@@ -1,42 +1,18 @@
-import jwt from "jsonwebtoken";
+import { findOnlineClients, sendMessages } from "./wsServices.js";
 
 export function handleConnection(ws, wss) {
   //ws is ur individual socket, wss is ur comeplete socket server
   ws.on("message", async (data) => {
     const message = JSON.parse(data);
-    if (message.type === "token") {//token part
-      try {
-        const { id, name } = jwt.verify(message.secret, process.env.SECRET);
-        ws.id = id;
-        ws.name = name;
-        console.log(
-          [...wss.clients].map((c) => `UserID: ${c.id}, Name: ${c.name}`)
-        );
-        [...wss.clients].forEach((client) => {
-          const otherClients = [...wss.clients]
-            .filter((c) => c !== client)
-            .map((c) => ({ id: c.id, name: c.name }));
-          console.log("Other clients: ", otherClients);
-          client.send(
-            JSON.stringify({
-              type: "onlineClient",
-              clients: otherClients,
-            })
-          );
-        });
-      } catch (error) {
-        console.log(error);
-      }
+    if (message.type === "token") {
+      //token part
+      console.log("Finding online client in be");
+      findOnlineClients(message, ws, wss);
     }
-    if (message.type === "message") {//sending message part
-      const { recipient, text, sender } = message;
-      [...wss.clients]
-        .filter((client) => client.id === recipient.id)
-        .forEach((client) =>
-          client.send(JSON.stringify({ type: "message", text: text, id: sender.id, name:sender.name }))
-        );
-
-      console.log("Fe message ", message);
+    if (message.type === "message") {
+      //sending message part
+      console.log("Sending message to client in be");
+      sendMessages(message, wss);
     }
     console.log("Fe message in backend ", message);
   });
