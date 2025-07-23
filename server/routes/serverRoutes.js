125f53e6f2f85bf17321398f33eb5a2c9af81085
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-24 01:47:24 +0530

    added getMessages route to fetch message history

--- server/routes/serverRoutes.js
+++ server/routes/serverRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {handleLogin, handleRegister} from "../controllers/register.js";
+import { handleLogin, handleRegister } from "../controllers/register.js";
+import { getMessages } from "../controllers/messages.js";
 
-const router=Router();
+const router = Router();
 
-router.post("/register",handleRegister);
+router.post("/register", handleRegister);
 router.post("/login", handleLogin);
+router.get("/messages/:userId", getMessages);
 
 export default router;
