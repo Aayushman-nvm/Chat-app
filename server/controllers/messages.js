fe8813494a34c2106162723057c242773944333e
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-25 00:54:40 +0530

    sending message history based on sender and recipient

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,4 +1,22 @@
-export function getMessages(req, res){
-    const {userId}=req.params
-console.log("UserId in be: ",userId);
-};
+import jwt from "jsonwebtoken";
+import { messageModel } from "../model/message.js";
+
+export async function getMessages(req, res) {
+  const { userId } = req.params;
+  const authHeader = req.headers.authorization;
+  const authToken = authHeader.split(" ");
+  const token = authToken[1];
+  const { id: ourId, name } = jwt.verify(token, process.env.SECRET);
+  console.log("ID, name: ", ourId, name);
+  const messages = await messageModel
+    .find({
+      $or: [
+        { "sender.id": userId, "recipient.id": ourId },
+        { "sender.id": ourId, "recipient.id": userId },
+      ],
+    })
+    .sort({ createdAt: 1 });
+    console.log("Message History: ",messages);
+    res.json(messages);
+  console.log("UserId in be: ", userId);
+}
