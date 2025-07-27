5afa49f0e9e03211a45d0cbf8f4b2716a40569f0
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-27 22:51:15 +0530

    logout and users

--- server/routes/serverRoutes.js
+++ server/routes/serverRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { handleLogin, handleRegister } from "../controllers/register.js";
-import { getMessages } from "../controllers/messages.js";
+import { handleLogin, handleRegister, handleLogout } from "../controllers/register.js";
+import { getMessages, getUsers } from "../controllers/application.js";
 
 const router = Router();
 
 router.post("/register", handleRegister);
 router.post("/login", handleLogin);
+router.post("/logout", handleLogout);
 router.get("/messages/:userId", getMessages);
+router.get("/users", getUsers);
 
 export default router;
