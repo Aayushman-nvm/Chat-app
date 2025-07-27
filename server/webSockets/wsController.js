3fbd69a9ec80a3778be310882f13e493632c5a7c
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-27 22:52:38 +0530

    Display offline users automatically

--- server/webSockets/wsController.js
+++ server/webSockets/wsController.js
@@ -1,6 +1,25 @@
-import { findOnlineClients, sendMessages } from "./wsServices.js";
+import { findOnlineClients, sendMessages, otherClients } from "./wsServices.js";
 
 export function handleConnection(ws, wss) {
+  ws.isAlive = true;
+  ws.timer = setInterval(() => {
+    ws.ping();
+    /*if theres no pong as response, trigger deathtimer that is 
+     trigger death timer to trigger socket termination*/
+    ws.deathTimer = setTimeout(() => {
+      ws.isAlive = false;
+      clearInterval(ws.timer);
+      ws.terminate();
+      //update online client list after socket termination
+      otherClients(wss);
+    }, 1000);
+  }, 5000);
+
+  ws.on("pong", () => {
+    clearTimeout(ws.deathTimer);
+    /*if theres pong as response, clear deathtimer that is 
+     dont trigger death timer to trigger socket termination*/
+  });
   //ws is ur individual socket, wss is ur comeplete socket server
   ws.on("message", async (data) => {
     const message = JSON.parse(data);
